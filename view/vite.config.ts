12300ab90f1fdd30ecb3c86b674280c5990e58ad
Manuel Nunes <manuel_s_nunes@yahoo.com>
2023-11-27 08:13:43 -0800

    update vite.config.js to allow dev/prod deployment if the deployed site has a "path" in the url

--- view/vite.config.ts
+++ view/vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    open: true,
-    port: 5173
-  },
-  base: "code-quiz-jsx",
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  console.log(command, mode);
+  return {
+    server: {
+      open: true,
+      port: 5173,
+    },
+    base: mode === "production" ? "/code-quiz-jsx/" : "/",
+    plugins: [react()],
+  };
+});
